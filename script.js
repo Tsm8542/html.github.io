2f57ca838e944ce6a42bba16cc81e9563966dbf5
Tsm8542 <shaikhabid932@gmail.com>
2022-08-11 19:55:15 +0530

    11-08-22 , 19:55

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("body").ready(function(){
-    $(".t-others,.ta,.tb,.tc,.td,.te,.tf,.th,.ti,.tk,.tl,.tm,.tn,.to,.tp,.tq,.tr,.ts,.tt,.tu,.tv,.tw").css("display","none");
+    $(".t-others,.ta,.tb,.tc,.td,.te,.tf,.th,.ti,.tk,.tl,.tm,.tn,.to,.tp,.tq,.tr,.ts,.tt,.tu,.tv,.tw").hide();
 })
 
 function display(param){
@@ -9,5 +9,16 @@ function display(param){
     }
     else{
         temp.style.display="none";
+    }
+}
+
+function t(){
+    if($("#tbtn").text()==="Show all"){
+        $(".t-others,.ta,.tb,.tc,.td,.te,.tf,.th,.ti,.tk,.tl,.tm,.tn,.to,.tp,.tq,.tr,.ts,.tt,.tu,.tv,.tw").show();
+        $("#tbtn").text("Hide all")
+    }
+    else{
+        $(".t-others,.ta,.tb,.tc,.td,.te,.tf,.th,.ti,.tk,.tl,.tm,.tn,.to,.tp,.tq,.tr,.ts,.tt,.tu,.tv,.tw").hide();
+        $("#tbtn").text("Show all");
     }
 }
